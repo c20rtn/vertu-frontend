49594def0f8cb265a33c7aab71680d5d1bca8722
Jack Corton <jack.corton@googlemail.com>
2023-09-19 10:00:55 +0100

    js files for mobile modals and functions

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const mobileMenuButton = document.getElementById("mobileMenuButton"),
+    mobileOverlayClose = document.getElementById("mobileOverlayClose"),
+    closeSearchModal = document.getElementsByClassName("close")[0],
+    modal = document.getElementById('searchModal');
+
+searchIcon.onclick = function () {
+    modal.style.display = "block";
+}
+
+closeSearchModal.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//MOBILE OVERLAY
+function openNav() {
+    document.getElementById("mobileOverlay").style.height = "100%";
+}
+function closeNav() {
+    document.getElementById("mobileOverlay").style.removeProperty("height");
+}
+mobileMenuButton.onclick = function () {
+    openNav();
+}
+mobileOverlayClose.onclick = function () {
+    closeNav();
+}
